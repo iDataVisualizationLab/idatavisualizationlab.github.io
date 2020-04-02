e751a9912e4f0f8716a868a3ac3af54283c0bd84
nvtngan <nvtngan@hcmus.edu.vn>
2020-04-01 19:55:54 -0500

    Update conference.js

--- js/conference.js
+++ js/conference.js
@@ -5,7 +5,7 @@ d3.tsv('data/conferences.tsv',function(e,data){
         .html(d=>`<td align="left" style="padding:5px;"><a
                     href="${d.Link}">${d.Name}</a></td>
             <td align="left" style="padding:5px;">${d.Acceptance}</td>
-            <td align="left" style="padding:5px;">${d.When}</td>
+            <td align="left" style="padding:5px;">${new Date(d.When)?d.When:d.When}</td>
             <td align="left" style="padding:5px;">${d.Where}</td>
             <td align="left" style="padding:5px;">${d.Dealine}</td>
             <td align="left" style="padding:5px;">${d.Notification}</td>
